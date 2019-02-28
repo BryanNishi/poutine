b3d18ab885bde937ef33d1278f78b6a3c58e7863
Bryan Nishi <Bryan.Nishi@gmail.com>
2019-02-27 18:13:55 -0700

    maps and date picker working

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//google maps API
+
+var map;
+
+function initMap() {
+
+    // The map, centered at zip from client input(latlng)
+    var bounds = new google.maps.LatLngBounds();
+    var latlng = new google.maps.LatLng(39.750, -105.000);
+    map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 9,
+            center: latlng
+        })
+
+};
+
+//datepicker
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
